0af977baa47e386786576d80dee63cc94ecd2c6f
EC2 Default User <ec2-user@ip-172-31-92-27.ec2.internal>
2021-03-07 08:40:10 +0000

    [Finish]ruby-7

---
+++ lesson7-2.rb
@@ -0,0 +1,28 @@
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか？"
+
+i = gets.to_i
+
+number = 1
+while number <= i do
+  puts "#{number}回目の計算"
+  puts "2つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+
+  puts "a+b=#{a + b}"
+  puts "a-b=#{a - b}"
+  puts "a*b=#{a * b}"
+  puts "a/b=#{a / b}"
+
+  number += 1
+end
+
+puts "計算を終了します"
+
